00f0a153ec8f5ce2fe0cf2a8071094fb1470e5d4
Ramón Álvarez <86166683+ralvarezdev@users.noreply.github.com>
2024-10-27 08:11:25 -0400

    feat: Added MongoDB schemas
    * Added JwtAccessToken, JwtAccessTokenLog, JwtRefreshToken, JwtRefreshTokenLog, Permission, RolePermission, Role, UserRole, UserLogInAttempt schemas

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/pixel-plaza-dev/uru-databases-2-auth-service
+
+go 1.23.2
--- src/config/microservice.ts
+++
@@ -1,6 +0,0 @@
-// User Microservice configuration
-export const IS_PRODUCTION = process.env.NODE_ENV === 'production';
-export const AUTH_MICROSERVICE_PORT = parseInt(
-    process.env.AUTH_MICROSERVICE_PORT
-);
-
